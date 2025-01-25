3ccb1acf376ac8bb397355677c2bcd6b537c39e8
zugdev <zug.dev@proton.me>
2025-01-25 04:41:34 -0300

    feat: try catch

--- src/directory/get-partner-avatars.ts
+++ src/directory/get-partner-avatars.ts
@@ -1,12 +1,17 @@
 import { GitHubOrganization, octokit } from "./directory";
 
 export async function getPartnerAvatars(ownerName: string): Promise<{ownerName: string, avatar_url?: string}> {
-  const orgResp: GitHubOrganization[] = await octokit.paginate({
-    method: "GET",
-    url: `/orgs/${ownerName}`
-  });
+  try {
+    const orgResp: GitHubOrganization[] = await octokit.paginate({
+      method: "GET",
+      url: `/orgs/${ownerName}`
+    });
 
-  const org = orgResp.find((org) => org.login === ownerName);
+    const org = orgResp.find((org) => org.login === ownerName);
 
-  return {ownerName, avatar_url: org ? org.avatar_url : undefined};
+    return {ownerName, avatar_url: org ? org.avatar_url : undefined};
+  } catch (error) {
+    console.error(`Error fetching organization for ${ownerName}:`, error);
+    return {ownerName, avatar_url: undefined};
+  }
 }
